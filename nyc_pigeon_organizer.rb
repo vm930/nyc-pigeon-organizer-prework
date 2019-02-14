2b06968e220cfe7db46e779876ca1c188aea060a
abstract-animation-9914 <vm930@hotmail.com>
2019-02-14 15:47:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,30 +1,30 @@
 
 require "pry"
 
-# def nyc_pigeon_organizer(data)
-#   new_hash = {}
+def nyc_pigeon_organizer(data)
+  new_hash = {}
   
-#   data.each do 
-#     |key1, value1| # :color => {}
-#     value1.each do 
-#       |key2, value2| #:purple => []
-#       value2.each do |value_str|  #"theo"
-#         #new_hash[value_str] = {key1 =>[]}
-#         if !(new_hash.has_key?(value_str))
-#           new_hash[value_str] = {key1 =>[value2.to_s]}
-#           #binding.pry
-#         else
-#           if !(new_hash[value_str].has_key?(key1))
-#             new_hash[value_str][key1] = [key2.to_s]
-#           else 
-#             new_hash[value_str][key1] << key2.to_s
-#           end 
-#       end
-#     end 
-#     end
-#   end
-#   new_hash
-# end 
+  data.each do 
+    |key1, value1| # :color => {}
+    value1.each do 
+      |key2, value2| #:purple => []
+      value2.each do |value_str|  #"theo"
+        #new_hash[value_str] = {key1 =>[]}
+        if !(new_hash.has_key?(value_str))
+          new_hash[value_str] = {key1 =>[value2.to_s]}
+          #binding.pry
+        else
+          if !(new_hash[value_str].has_key?(key1))
+            new_hash[value_str][key1] = [key2.to_s]
+          else 
+            new_hash[value_str][key1] << key2.to_s
+          end 
+      end
+    end 
+    end
+  end
+  new_hash
+end 
 
 
 # i think i might need help with this :(((
