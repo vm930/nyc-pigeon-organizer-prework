347189aa16239fb1f2f8d46a74e07929814bf31a
abstract-animation-9914 <vm930@hotmail.com>
2019-02-14 15:50:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -27,31 +27,31 @@ def nyc_pigeon_organizer(data)
 end 
 
 
-def nyc_pigeon_organizer(data)
-  # write your code here!
-  new_data = {}
+# def nyc_pigeon_organizer(data)
+#   # write your code here!
+#   new_data = {}
 
-  data.each do |key_one, val_one|
-    val_one.each do |key_two, val_two|
-      val_two.each do |el|
-        # new_data[el] = {key_one=>[key_two.to_s]}
-        if !(new_data.has_key?(el))
-          new_data[el] = {key_one=>[key_two.to_s]}
-          # binding.pry
-        else
-          if !(new_data[el].has_key?(key_one))
-            new_data[el][key_one] = [key_two.to_s]
-            # binding.pry
-          else
-            new_data[el][key_one] << key_two.to_s
-            # binding.pry
-          end
-        end
-      end
-    end
-  end
-  new_data
-end
+#   data.each do |key_one, val_one|
+#     val_one.each do |key_two, val_two|
+#       val_two.each do |el|
+#         # new_data[el] = {key_one=>[key_two.to_s]}
+#         if !(new_data.has_key?(el))
+#           new_data[el] = {key_one=>[key_two.to_s]}
+#           # binding.pry
+#         else
+#           if !(new_data[el].has_key?(key_one))
+#             new_data[el][key_one] = [key_two.to_s]
+#             # binding.pry
+#           else
+#             new_data[el][key_one] << key_two.to_s
+#             # binding.pry
+#           end
+#         end
+#       end
+#     end
+#   end
+#   new_data
+# end
 
 
 
