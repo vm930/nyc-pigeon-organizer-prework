bd897a2c3b8c9b444091d3821eac96838a7c6c44
abstract-animation-9914 <vm930@hotmail.com>
2019-02-14 15:47:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -59,25 +59,3 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-# def nyc_pigeon_organizer(data)
-#   data.each_with_object({}) do |(key, h), result|
-#     h.each do |value, names|
-#       names.each do |name|
-#         result[name] ||= {}
-#         result[name][key] ||= []
-#         result[name][key] << value.to_s
-#       end
-#     end
-#   end
-# end
